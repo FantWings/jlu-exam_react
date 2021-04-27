482606c88f938483847c89b76b8ce13aa3dc9f37
fantwings <zhijun@furry.top>
2021-04-27 17:46:58 +0400

    调整路由规则

--- src/router.jsx
+++ src/router.jsx
@@ -3,17 +3,18 @@ import Froms from './pages/PageIndex'
 import History from './pages/PageHistory'
 import PageAnswer from './pages/PageAnswer'
 import PageContainer from './pages'
-import { Route } from 'react-router'
+import { Route, Switch, Redirect } from 'react-router'
 
 export default function router() {
   //APP外壳函数
   return (
     <PageContainer>
-      <Route path="/history" component={History} strict={true} />
-      <Route path="/" component={Froms} exact={true} />
-      <Route path="/answer/:paper_id" component={PageAnswer} />
-      {/* <Route path="/answer/:paper_id" component={AnswerBody} />
-        <Route path="/(home|answer)" component={Forms} strict={true} /> */}
+      <Switch>
+        <Route path="/" component={Froms} exact />
+        <Route path="/history" component={History} exact />
+        <Route path="/answer/:paper_id" component={PageAnswer} exact/>
+        <Redirect to="/"/>
+      </Switch>
     </PageContainer>
   )
 }
