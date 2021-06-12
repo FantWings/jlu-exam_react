7908b04382362e2b06b2a6cd1042255fab0a6204
fantwings <zhijun@furry.top>
2021-06-12 07:09:36 +0400

    初始化默认语言

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import Router from './router'
+import { ConfigProvider } from 'antd'
+import zhCN from 'antd/lib/locale/zh_CN'
 
 // ReactRender入口函数
 ReactDOM.render(
   <BrowserRouter>
-    <Router />
+    <ConfigProvider locale={zhCN}>
+      <Router />
+    </ConfigProvider>
   </BrowserRouter>,
   document.querySelector('#root')
 )
