2873746fe7539d18193fd1fc834d01d1331f90b1
fantwings <zhijun@furry.top>
2021-06-16 09:57:15 +0400

    提升版本号

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -47,7 +47,7 @@ export default function PageContianer(props) {
         <Footer>
           <ul>
             <li>
-              版本 version 5.0（<a href="https://reactjs.org">ReactJS</a>）
+              版本 version 5.1（<a href="https://reactjs.org">ReactJS</a>）
             </li>
             <li>
               <span id={data.count ? 'connected' : 'disconnected'} className="ping" />
@@ -63,10 +63,6 @@ export default function PageContianer(props) {
     </Container>
   )
 }
-
-const Counter = styled.span`
-  color: orange;
-`
 
 const Container = styled.div`
   height: 100%;
@@ -98,6 +94,10 @@ const PageTitle = styled.div`
       margin: 0 5px;
     }
   }
+`
+
+const Counter = styled.span`
+  color: orange;
 `
 
 const Footer = styled.footer`
