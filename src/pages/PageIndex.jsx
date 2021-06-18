ddfe0d74808fa3f8477efa57dfd4c45bc14612eb
FantWings <zhijun@furry.top>
2021-06-18 04:38:11 +0400

    优化判断逻辑和按钮样式

--- src/pages/PageIndex.jsx
+++ src/pages/PageIndex.jsx
@@ -1,48 +1,37 @@
 import { useState } from 'react'
 import { useHistory } from 'react-router'
-import { Input, message } from 'antd'
+import { Input, message, Button } from 'antd'
 import styled from 'styled-components'
 import { BASE_URL } from '../config'
 import { fetchData } from '../hooks/useFetch'
 
 export default function PageForms() {
   const [FromData, setFromData] = useState(0)
-  const [status, setStatus] = useState({
-    status: 'ready',
+  const [buttomStatus, setButtomStatus] = useState({
+    disable: true,
+    failure: false,
+    loading: false,
     text: '提交',
   })
 
   const history = useHistory()
   const { TextArea } = Input
+  
 
   const handleClick = async (e) => {
     e.preventDefault()
 
-    //判断用户是否输入了内容
-    if (FromData.length >= 10000) {
-      message.loading({ content: '数据处理中，请稍等....', key: 'statusbar', duration: 0 })
-      setStatus({ status: 'sending', text: '别着急' })
-    } else {
-      message.warning({
-        content: '您没有填写试卷数据或数据长度不正确，请检查是否提交了错误的数据！',
-        key: 'statusbar',
-      })
-      return setStatus({ status: 'failure', text: '重试一次' })
-    }
+    if (!FromData) return message.warn('提交的数据不可为空，请填写后重试')
 
     //判断用户是否输入的是错误的数据
     try {
       var req = { question_data: JSON.parse(FromData) }
     } catch (error) {
-      message.warning({
-        content: '你输入的试卷数据不正确或试卷数据不完整，请检查！',
-        key: 'statusbar',
-      })
-      return setStatus({ status: 'failure', text: '重试一次' })
+      message.warning('你输入的数据不正确或不完整，请检查后重新提交')
+      return setButtomStatus({ ...buttomStatus, failure: true, text: '重试一次' })
     }
 
     const { answerPaperRecordId } = req.question_data.data
-
     const { code } = await fetchData(`${BASE_URL}/paper/${answerPaperRecordId}`, {
       method: 'POST',
       headers: {
@@ -53,12 +42,11 @@ export default function PageForms() {
     })
 
     if (code) {
-      return setStatus({ status: 'failure', text: '重试一次' })
+      return setButtomStatus({ ...buttomStatus, failure: true, text: '重试一次' })
     } else {
       history.push(`/answer/${answerPaperRecordId}`)
       //重定向用户浏览器视口到顶部
       window.scrollTo(0, 0)
-      message.destroy('statusbar')
     }
   }
 
@@ -68,13 +56,21 @@ export default function PageForms() {
         showCount
         placeholder="将获取到的试卷数据粘贴到这里，如果您不清楚如何获取数据，请点击菜单上的“使用教程”"
         autoSize={{ minRows: 20, maxRows: 20 }}
-        onChange={(e) => setFromData(e.target.value)}
+        onChange={(e) => setFromData(e.target.value) }
         allowClear
       />
       <div id="key_feid">
-        <button onClick={(e) => handleClick(e, FromData)} id="submit" className={status.status}>
-          {status.text}
-        </button>
+        <Button
+          onClick={(e) => handleClick(e, FromData)}
+          type="primary"
+          shape="round"
+          size="large"
+          style={{ padding: '0 80px' }}
+          danger={buttomStatus.failure}
+          loading={buttomStatus.loading}
+        >
+          {buttomStatus.text}
+        </Button>
       </div>
     </Form>
   )
@@ -108,40 +104,5 @@ const Form = styled.form`
 
   #form_contain {
     flex: 100%;
-  }
-
-  #submit {
-    cursor: pointer;
-    outline: none;
-    border: 0;
-    padding: 10px 80px;
-    border-radius: 999em;
-    background: #5882dd;
-    color: #fff;
-    margin: auto;
-    transition: 0.3s;
-    &:hover {
-      background: #6596ff;
-      transform: scale(1.02);
-    }
-
-    &:active {
-      transform: scale(0.98);
-      background: #4a6fbe;
-    }
-    &.sending {
-      background: #b8ceff;
-      cursor: not-allowed;
-      pointer-events: none;
-    }
-    &.failure {
-      background: #fa4e4e;
-    }
-    &.failure:hover {
-      background: #ff8080;
-    }
-    &.failure:active {
-      background: #ca6565;
-    }
   }
 `
